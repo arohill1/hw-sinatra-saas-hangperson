8a636c7ade92f403246cd202ed043f494d8e3699
Ubuntu <ubuntu@ip-172-31-89-105.ec2.internal>
2020-02-20 01:58:53 +0000

    Ready for Heroku!

--- lib/hangperson_game.rb
+++ lib/hangperson_game.rb
@@ -2,52 +2,52 @@ class HangpersonGame
 
   # add the necessary class methods, attributes, etc. here
   # to make the tests in spec/hangperson_game_spec.rb pass.
-
+  
+  LOSE = 7
+  
   # Get a word from remote "random word" service
 
   # def initialize()
   # end
   
-  attr_accessor :word, :guesses, :wrong_guesses, :play, :win, :lose
+  
   
   def initialize(word)
     @word = word
     @guesses = ''
     @wrong_guesses = ''
+    @message = ''
+    
   end
   
-  def guess letter
-    raise ArgumentError if 
-      letter.nil? or letter.empty? or letter !~ /[A-Za-z]/ 
-    
+  attr_accessor :word
+  attr_accessor :guesses
+  attr_accessor :wrong_guesses
+  
+  def guess(letter)
+    raise ArgumentError if  letter.nil? || letter.empty? || letter.match(/[^a-zA-Z]/)
     letter.downcase!
-    return false if guesses.include? letter or wrong_guesses.include? letter
-    
+    return false if 
+      (guesses.include? letter) || (wrong_guesses.include? letter)
     if word.include? letter
-      @guesses << letter
+      guesses << letter
     else
-      @wrong_guesses << letter
+      wrong_guesses << letter
     end
     return true
+  end
+  
+  def check_win_or_lose
+    return :lose if wrong_guesses.length >= LOSE
+    word_with_guesses == word ? :win : :play 
   end
   
   def word_with_guesses
     word.gsub(/./) do |letter|
       (guesses.include? letter)? letter : "-"
     end
-  end  
-  
-  def check_win_or_lose
-      if word_with_guesses == @word
-        :win
-      elsif wrong_guesses.chars.count >= 7
-        :lose
-      else
-        :play
-      end 
   end
-
- 
+    
   # You can test it by running $ bundle exec irb -I. -r app.rb
   # And then in the irb: irb(main):001:0> HangpersonGame.get_random_word
   #  => "cooking"   <-- some random word
